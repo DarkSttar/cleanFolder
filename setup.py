283e73170ffb674e87bdada701f38203dd0fb3f2
DarkSttar <llldarklllstarlll@gmail.com>
2023-10-24 00:51:36 +0300

    com1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup,find_namespace_packages
 setup(
     name='cleanfolder',
     version='1.0.0',
-    description='Sorting and cleaning Directory',
+    description='cleaner',
     author='Dark Star',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': ['clean_folder=cleanfolder.main:start']}
